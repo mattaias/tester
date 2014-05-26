93ae67326a6b61f8e351d7d8025879502790b778
mattaias <mathiasclaeys@hotmail.com>
2014-05-26 12:16:15 +0200

    test commit 01

---
+++ SetUp_GIT.bat
@@ -0,0 +1,9 @@
+ECHO OFF
+
+SET USERNAME="mclaeys"
+SET USEREMAIL=mclaeys@walkingthedog.be
+
+git config --global user.name %USERNAME%
+git config --global user.email %USEREMAIL%
+REM git config --global core.editor emacs
+REM git config --global merge.tool vimdiff
