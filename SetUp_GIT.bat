fcde2011bb55cb2400f666105577754e2d05ce8e
mattaias <mathiasclaeys@hotmail.com>
2014-05-26 12:20:46 +0200

    test01

--- SetUp_GIT.bat
+++ SetUp_GIT.bat
@@ -7,3 +7,4 @@ git config --global user.name %USERNAME%
 git config --global user.email %USEREMAIL%
 REM git config --global core.editor emacs
 REM git config --global merge.tool vimdiff
+REM git config --global merge.tool vimdiff
