d6631e25f3ccacd4f803de64681925e899826765
mattaias <mathiasclaeys@hotmail.com>
2014-05-26 15:12:02 +0200

    Added UI-test

--- Main Project/Code.py
+++ Main Project/Code.py
@@ -1,2 +1,28 @@
 print "Only basic code"
 print "Code for further test..."
+
+import sys
+import os
+from PyQt4 import QtCore, QtGui, uic
+
+thisFolder = os.path.dirname(os.path.realpath(__file__))
+uiName = "Ok_Test.ui"
+uiPath = "%s\%s" %(thisFolder,uiName)
+
+class Tester(QtGui.QWidget):
+	
+	def __init__(self,parent=None):
+		QtGui.QMainWindow.__init__(self, parent=parent)
+		uic.loadUi(uiPath, self)
+		
+		
+def main():		
+	app = QtGui.QApplication(sys.argv)
+	widget = Tester()
+	widget.show()
+
+	sys.exit(app.exec_())
+
+	
+if __name__ == '__main__':
+	main()
